a0efc4ed7fe9e0a18a02a61f71a579e698f4e1f5
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-06-22 23:38:23 -0300

    Add Colors, TextStyle, Images and Initial Pages

---
+++ lib/modules/login_page.dart/login_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/appcolors.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: [
+            Container(
+              width: size.width,
+              height: size.height * 0.40,
+              color: AppColors.primary,
+            ),
+            Positioned(
+                top: 40,
+                left: 0,
+                right: 0,
+                child: Image.asset(
+                  AppImages.person,
+                  width: 673,
+                  height: 408,
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
