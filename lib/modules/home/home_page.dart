67456731a1fb43fbe9195f8dfdc2269dc05bd61b
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-06-24 23:05:59 -0300

    Finished the Third Class

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,32 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_controller.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/themes/appcolors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+  final pages = [
+    Container(
+      color: Colors.white,
+    ),
+    Container(color: AppColors.primary),
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: PreferredSize(
-      preferredSize: Size.fromHeight(252),
-      child: Container(
-        height: 190,
-        color: AppColors.primary,
-        child: Center(
-          child: ListTile(
-            title: Text('Olá, João Paulo'),
-            subtitle: Text('mantenha suas contas em dia.'),
-            trailing: Container(
-              height: 56,
-              width: 56,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.circular(10),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(252),
+        child: Container(
+          height: 190,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                    text: 'Olá, ',
+                    style: TextStyles.titleRegular,
+                    children: [
+                      TextSpan(
+                        text: 'João Paulo',
+                        style: TextStyles.titleBoldBackground,
+                      ),
+                    ]),
+              ),
+              subtitle: Text('mantenha suas contas em dia.',
+                  style: TextStyles.captionShape),
+              trailing: Container(
+                height: 56,
+                width: 56,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
             ),
           ),
         ),
       ),
-    ));
+      body: pages[controller.currentPage],
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {
+                controller.setPage(0);
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.home,
+                color: AppColors.primary,
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                print('Clicou');
+              },
+              child: Container(
+                height: 56,
+                width: 56,
+                decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Icon(
+                  Icons.add_box_outlined,
+                  color: AppColors.background,
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                controller.setPage(1);
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.description_outlined,
+                color: AppColors.body,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
