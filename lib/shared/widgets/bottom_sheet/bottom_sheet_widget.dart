d612fe0439a136fec8585d8b4784d05c56dec41c
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-06-30 19:44:55 -0300

    Finish Layout Barcode Scanner

--- lib/shared/widgets/bottom_sheet/bottom_sheet_widget.dart
+++ lib/shared/widgets/bottom_sheet/bottom_sheet_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
 import 'package:payflow/shared/themes/appcolors.dart';
 import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons_widget.dart';
@@ -22,18 +23,53 @@ class BottomSheetWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AppColors.shape,
-      child: Column(
-        children: [
-          Text(title),
-          Text(subtitle),
-          SetLabelButton(
-              primaryLabel: primaryLabel,
-              primaryOnPressed: primaryOnPressed,
-              secundaryLabel: secundaryLabel,
-              secundaryOnPressed: secundaryOnPressed)
-        ],
+    return SafeArea(
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Material(
+          child: Container(
+            color: AppColors.shape,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.black.withOpacity(0.6),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(40.0),
+                      child: Text.rich(
+                        TextSpan(
+                          text: title,
+                          style: TextStyles.buttonBoldHeading,
+                          children: [
+                            TextSpan(
+                              text: "\n$subtitle",
+                              style: TextStyles.buttonHeading,
+                            ),
+                          ],
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Container(
+                      height: 1,
+                      color: AppColors.stroke,
+                    ),
+                    SetLabelButton(
+                        enablePrimaryColor: true,
+                        primaryLabel: primaryLabel,
+                        primaryOnPressed: primaryOnPressed,
+                        secundaryLabel: secundaryLabel,
+                        secundaryOnPressed: secundaryOnPressed)
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/shared/widgets/set_label_buttons/set_label_buttons_widget.dart
+++ lib/shared/widgets/set_label_buttons/set_label_buttons_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
 import 'package:payflow/shared/widgets/divider_vertical/divider_vertical_widget.dart';
 import 'package:payflow/shared/widgets/label_button/label_button.dart';
@@ -8,6 +9,7 @@ class SetLabelButton extends StatelessWidget {
   final VoidCallback primaryOnPressed;
   final String secundaryLabel;
   final VoidCallback secundaryOnPressed;
+  final bool enablePrimaryColor;
 
   const SetLabelButton({
     Key? key,
@@ -15,6 +17,7 @@ class SetLabelButton extends StatelessWidget {
     required this.primaryOnPressed,
     required this.secundaryLabel,
     required this.secundaryOnPressed,
+    this.enablePrimaryColor = false,
   }) : super(key: key);
 
   @override
@@ -27,6 +30,7 @@ class SetLabelButton extends StatelessWidget {
             child: LabelButton(
               label: primaryLabel,
               onPressed: primaryOnPressed,
+              style: enablePrimaryColor ? TextStyles.buttonPrimary : null,
             ),
           ),
           DividerVerticalWidget(),
