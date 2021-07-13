6b798d50abfccfa8f319fd9b187a42a6b64bad52
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-07-12 23:52:40 -0300

    Validator Forms Editing

--- lib/modules/insert_boleto/insert_boleto_controller.dart
+++ lib/modules/insert_boleto/insert_boleto_controller.dart
@@ -1,10 +1,21 @@
+import 'package:flutter/material.dart';
+
 class InsertBoletoController {
+  final formkey = GlobalKey<FormState>();
+
   String? validateName(String? value) =>
       value?.isEmpty ?? true ? "O nome não pode ser vazio" : null;
   String? validateVencimento(String? value) =>
-      value?.isEmpty ?? true ? "A data de vencimento não pode ser vazio" : null;
+      value?.isEmpty ?? true ? "A data de vencimento não pode ser vazia" : null;
   String? validateValor(double value) =>
       value == 0 ? "Insira um valor maior que R\$ 0,00" : null;
   String? validateCodigo(String? value) =>
       value?.isEmpty ?? true ? "O código do boleto não pode ser vazio" : null;
+  
+  void onChange({String? name, String? dueDate, double? value, String? barcode})
+
+  void cadastrarBoleto() {
+    final form = formkey.currentState;
+    if (form!.validate()) {}
+  }
 }
